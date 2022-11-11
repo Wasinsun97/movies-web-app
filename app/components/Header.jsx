7122b0435bfc201d3ae92d572c66f3bdf8935228
Wasin Nimlaor <wasin.nimlaor@gmail.com>
2022-11-11 14:45:07 +0700

    Enhance Header's css

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
 import HeaderItem from "./HeaderItem";
@@ -8,19 +10,25 @@ import {
   RectangleStackIcon as CollectionIcon,
   FilmIcon,
 } from "@heroicons/react/24/outline";
+import { useRouter } from "next/navigation";
 
 const Header = () => {
+  const router = useRouter();
+
   return (
-    <div className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
-      <div className="flex flex-grow justify-evenly max-w-2xl">
+    <div className="flex flex-col-reverse sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-lg">
         <HeaderItem title="HOME" Icon={HomeIcon} />
         <HeaderItem title="SEARCH" Icon={SearchIcon} />
         <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
         <HeaderItem title="ACCOUNT" Icon={UserIcon} />
       </div>
-      <div className="flex flex-col items-center sm:mr-20">
-        <FilmIcon className="w-14" />
-        <h1 className="text-l text-bold italic">MOVIEFLIX</h1>
+      <div
+        className="flex flex-col items-center mb-5 sm:mr-20 sm:mb-0 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white"
+        onClick={() => router.push("/")}
+      >
+        <FilmIcon className="w-12" />
+        <h3 className="text-bold italic">MOVIEFLIX</h3>
       </div>
     </div>
   );
