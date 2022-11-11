83d9045e9b1d8db4e8e15255e783184d6aff88f7
Wasin Nimlaor <wasin.nimlaor@gmail.com>
2022-11-11 16:15:38 +0700

    change url query param from genre to tag

--- app/page.jsx
+++ app/page.jsx
@@ -2,17 +2,16 @@ import { useSearchParams } from "next/navigation";
 import apis, { BASE_URL } from "../utils/request";
 import Results from "./components/Results";
 
-const getData = async (genre) => {
-  const res = await fetch(
-    `${BASE_URL}${apis[genre]?.url || apis.trending.url}`,
-    { cache: "no-store" }
-  );
+const getData = async (tag) => {
+  const res = await fetch(`${BASE_URL}${apis[tag]?.url || apis.trending.url}`, {
+    cache: "no-store",
+  });
 
   return res.json();
 };
 
 export default async function Home({ params, searchParams }) {
-  const data = await getData(searchParams.genre);
+  const data = await getData(searchParams.tag);
 
   return (
     <div>
