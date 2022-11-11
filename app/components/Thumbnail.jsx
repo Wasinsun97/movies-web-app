2d867d4a6d3963a077635b5822974c36951e4adb
Wasin Nimlaor <wasin.nimlaor@gmail.com>
2022-11-11 16:16:05 +0700

    Create movie detail page

--- app/components/Thumbnail.jsx
+++ app/components/Thumbnail.jsx
@@ -1,11 +1,21 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
 import { HandThumbUpIcon } from "@heroicons/react/24/outline";
 import { IMAGE_BASE_URL } from "../../utils/request";
+import { useRouter } from "next/navigation";
 
 const Thumbnail = ({ result }) => {
+  const router = useRouter();
+
   return (
-    <div className="group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+    <div
+      className="group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50"
+      onClick={() =>
+        router.push(`/details/${result.id}?media=${result.media_type}`)
+      }
+    >
       <Image
         alt="thumbnail"
         src={`${IMAGE_BASE_URL}${result.backdrop_path}`}
@@ -21,7 +31,8 @@ const Thumbnail = ({ result }) => {
         <p className="flex items-center opacity-0 group-hover:opacity-100">
           {result.media_type && `${result.media_type} •`}{" "}
           {result.release_date || result.first_air_date} •{" "}
-          <HandThumbUpIcon className="h-5 mx-2" /> {result.vote_count}
+          <HandThumbUpIcon className="h-5 mx-2" />{" "}
+          {result.vote_count.toLocaleString()}
         </p>
       </div>
     </div>
---
+++ app/details/[id]/page.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import Image from "next/image";
+import { HandThumbUpIcon } from "@heroicons/react/24/outline";
+import { API_KEY, BASE_URL, IMAGE_BASE_URL } from "../../../utils/request";
+
+const fetchMovieById = async (id, mediaType) => {
+  let res;
+  res = await fetch(
+    `${BASE_URL}/${mediaType}/${id}?api_key=${API_KEY}&language=en-US`,
+    { cache: "no-store" }
+  );
+
+  if (!res.ok) {
+    res = await fetch(
+      `${BASE_URL}/movie/${id}?api_key=${API_KEY}&language=en-US`,
+      { cache: "no-store" }
+    );
+  }
+  return res.json();
+};
+
+const renderTags = (title, details) => (
+  <>
+    <p className="font-bold mt-4">{title}</p>
+    <p className="italic">
+      {details.map(
+        (item, index) =>
+          `${item.name} ${index < details.length - 1 ? "• " : ""}`
+      )}
+    </p>
+  </>
+);
+
+const DetailPage = async ({ params, searchParams }) => {
+  const data = await fetchMovieById(params.id, searchParams.media);
+
+  return (
+    <div>
+      <div className="p-2">
+        <h2 className="text-2xl font-bold">
+          {data.title || data.original_name}
+        </h2>
+      </div>
+      <Image
+        alt="thumbnail"
+        className="mt-4"
+        src={`${IMAGE_BASE_URL}${data.backdrop_path}`}
+        height={1080}
+        width={1920}
+        loading="lazy"
+      />
+      <p className="flex items-center mt-4 italic">
+        {data.revenue ? `$${data.revenue.toLocaleString()} USD •` : ""}{" "}
+        {data.release_date || data.first_air_date} •{" "}
+        <HandThumbUpIcon className="h-5 mx-2" />{" "}
+        {data.vote_count.toLocaleString()}
+      </p>
+      {renderTags("Genre", data.genres)}
+      {renderTags("Studio", data.production_companies)}
+      {data.budget ? (
+        <>
+          <p className="font-bold mt-4">Budget</p>
+          <p className="italic">{`$${data.budget.toLocaleString()} USD`}</p>
+        </>
+      ) : undefined}
+      <p className="font-bold mt-4">Overview</p>
+      <p>{data.overview}</p>
+    </div>
+  );
+};
+
+export default DetailPage;
