dba45ece14d6828d775a122eef2c7e84d0ddc289
Wasin Nimlaor <wasin.nimlaor@gmail.com>
2022-12-22 15:21:01 +0700

    Apply handleOnKeydown when search

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -21,6 +21,10 @@ const SearchPage = () => {
     });
   };
 
+  const handleOnKeydown = (e) => {
+    if (e.key === "Enter") handleSearch();
+  };
+
   return (
     <div className="px-20 my-10">
       <div className="grid grid-cols-3 gap-4 grid-flow-row">
@@ -28,6 +32,7 @@ const SearchPage = () => {
           type="text"
           className="rounded-md h-10 col-span-2 text-gray-700 p-2.5"
           ref={ref}
+          onKeyDown={handleOnKeydown}
         />
         <button
           className="rounded-md h-10 col-span-1 bg-gradient-to-r from-cyan-500 to-blue-500 hover:from-green-500 hover:to-cyan-500"
