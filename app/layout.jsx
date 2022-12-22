d810e870beb250b5befb1908fec73522d33dbcda
Wasin Nimlaor <wasin.nimlaor@gmail.com>
2022-12-22 18:21:25 +0700

    Apply log in with google

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,27 @@
+"use client";
+
 import "../styles/globals.css";
 import Nav from "./components/Nav";
 import Header from "./components/Header";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../utils/firebase";
 
 export default function RootLayout({ children }) {
+  const [user] = useAuthState(auth);
+
   return (
     <html>
       <body>
         <>
           <title>MovieFlix</title>
         </>
-        <Header />
-        <Nav />
+        {user ? (
+          <>
+            <Header />
+            <Nav />
+          </>
+        ) : null}
+
         <>{children}</>
       </body>
     </html>
---
+++ app/login/page.jsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../../utils/firebase";
+import { useRouter } from "next/navigation";
+
+const LoginPage = () => {
+  const router = useRouter();
+  const googleProvider = new GoogleAuthProvider();
+
+  const handleGoogleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+      if (result.user) router.push("/");
+    } catch (error) {
+      console.log("error===>", error);
+    }
+  };
+
+  return (
+    <div className="shadow-cyan-800 shadow-xl m-32 p-10 bg-white text-gray-700 rounded-lg">
+      <div className="flex flex-col items-center">
+        <h1 className="text-2xl mb-8">Sign in</h1>
+        <button
+          className="flex items-center rounded-md h-10 col-span-1 bg-gradient-to-r from-cyan-800 to-blue-800 hover:from-green-800 hover:to-cyan-800 p-8"
+          onClick={handleGoogleLogin}
+        >
+          <Image src="/google.svg" alt="google" width={20} height={20} />
+          <p className="ml-2 text-white">Sign in with Google</p>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
