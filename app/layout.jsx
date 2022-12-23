bf40d1d465545dcaedac59110a95a59bf6af11bb
Wasin Nimlaor <wasin.nimlaor@gmail.com>
2022-12-23 14:23:16 +0700

    enhance user authentication logic

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,9 +5,18 @@ import Nav from "./components/Nav";
 import Header from "./components/Header";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../utils/firebase";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function RootLayout({ children }) {
+  const pathname = usePathname();
+  const router = useRouter();
   const [user] = useAuthState(auth);
+
+  useEffect(() => {
+    if (!user) router.push("/login");
+  }, [router, user]);
 
   return (
     <html>
@@ -18,7 +27,7 @@ export default function RootLayout({ children }) {
         {user ? (
           <>
             <Header />
-            <Nav />
+            {pathname !== "/profile" ? <Nav /> : null}
           </>
         ) : null}
 
